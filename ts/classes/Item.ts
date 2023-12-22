05b7bea7625382ad3c441522058277d38ca759f6
Yafarrodrigo <yafarrodrigo@yahoo.com.ar>
2023-12-22 00:59:52 -0300

    item.ts

---
+++ ts/classes/Item.ts
@@ -0,0 +1,12 @@
+export default class Item{
+
+    id: string
+    type: string
+    durability: number
+
+    constructor(){
+        this.id = "id"
+        this.type = "type"
+        this.durability = 1
+    }
+}
