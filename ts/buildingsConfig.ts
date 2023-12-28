085e7071f6e0f4334db9ca4e76ef4b338a7ab3c1
Yafarrodrigo <yafarrodrigo@yahoo.com.ar>
2023-12-27 22:12:23 -0300

    changed buildings config

--- ts/buildingsConfig.ts
+++ ts/buildingsConfig.ts
@@ -1,117 +1,177 @@
 export type buildingInfo = {
     id: string
     weight: number
-    areas: {[key:string]:{min:number,max:number}}
+    areas: {id:string,min:number,max:number}[]
+    entrances:{id: string, min:number, max:number}[]
 }
 
 const buildingsConfig: buildingInfo[] = [
     {
         id:"smallHouse",
         weight:100,
-        areas:{
-            livingRoom:{min: 0,max: 1},
-            kitchen:{min: 1,max: 1},
-            bathroom:{min: 1,max: 1},
-            bedroom:{min: 1,max: 2},
-            garden:{min: 0,max: 1},
-            garage:{min: 0,max: 1}
-        }
+        areas:[
+            {id:"livingRoom",min: 0,max: 1},
+            {id:"kitchen",min: 1,max: 1},
+            {id:"bathroom",min: 1,max: 1},
+            {id:"bedroom",min: 1,max: 2},
+            {id:"garden",min: 0,max: 1},
+            {id:"garage",min: 0,max: 1}
+        ],
+        entrances:[
+            {id: "frontDoor", min: 1, max: 1},
+            {id: "backDoor", min: 0, max: 1},
+            {id: "garageDoor", min: 0, max: 1},
+            {id: "smallWindow", min: 1, max: 8},
+            {id: "largeWindow", min: 0, max: 1},
+        ]
     },
     {
         id:"bigHouse",
         weight:100,
-        areas:{
-            livingRoom:{min: 1,max: 1},
-            kitchen:{min: 1,max: 1},
-            bathroom:{min: 1,max: 2},
-            bedroom:{min: 2,max: 4},
-            garden:{min: 1,max: 1},
-            gardeningShack:{min: 0,max: 1},
-            garage:{min: 1,max: 1}
-        }
+        areas:[
+            {id:"livingRoom",min: 1,max: 1},
+            {id:"kitchen",min: 1,max: 1},
+            {id:"bathroom",min: 1,max: 2},
+            {id:"bedroom",min: 2,max: 4},
+            {id:"garden",min: 1,max: 1},
+            {id:"gardeningShack",min: 0,max: 1},
+            {id:"garage",min: 1,max: 1}
+        ],
+        entrances:[
+            {id: "frontDoor", min: 1, max: 1},
+            {id: "backDoor", min: 1, max: 1},
+            {id: "garageDoor", min: 1, max: 1},
+            {id: "smallWindow", min: 1, max: 12},
+            {id: "largeWindow", min: 0, max: 4}
+        ]
     },
     {
         id:"clothingShop",
         weight:25,
-        areas:{
-            clothesStorageRoom:{min: 1,max: 1},
-            toilet:{min: 0,max: 2},
-            parking:{min: 0,max: 1},
-            offices:{min: 1,max: 1}
-        }
+        areas:[
+            {id:"clothesStorageRoom",min: 1,max: 1},
+            {id:"toilet",min: 0,max: 2},
+            {id:"parking",min: 0,max: 1},
+            {id:"offices",min: 1,max: 1}
+        ],
+        entrances:[
+            {id: "frontDoor", min: 1, max: 1},
+            {id: "backDoor", min: 0, max: 1},
+            {id: "smallWindow", min: 0, max: 2},
+            {id: "largeWindow", min: 1, max: 1}
+        ]
     },
     {
         id:"toolsShop",
         weight:25,
-        areas:{
-            toolsStorageRoom:{min: 1,max: 1},
-            toilet:{min: 0,max: 2},
-            parking:{min: 0,max: 1},
-            offices:{min: 1,max: 1}
-        }
+        areas:[
+            {id:"toolsStorageRoom",min: 1,max: 1},
+            {id:"toilet",min: 0,max: 2},
+            {id:"parking",min: 0,max: 1},
+            {id:"offices",min: 1,max: 1}
+        ],
+        entrances:[
+            {id: "frontDoor", min: 1, max: 1},
+            {id: "backDoor", min: 0, max: 1},
+            {id: "smallWindow", min: 0, max: 2},
+            {id: "largeWindow", min: 1, max: 1}
+        ]
     },
     {
         id:"mechanicShop",
         weight:25,
-        areas:{
-            mechanicStorage:{min: 1,max: 1},
-            garage:{min: 1,max: 1},
-            toilet:{min: 0,max: 2},
-            offices:{min: 1,max: 1}
-        }
+        areas:[
+            {id:"mechanicStorage",min: 1,max: 1},
+            {id:"garage",min: 1,max: 1},
+            {id:"toilet",min: 0,max: 2},
+            {id:"offices",min: 1,max: 1}
+        ],
+        entrances:[
+            {id: "frontDoor", min: 1, max: 1},
+            {id: "backDoor", min: 0, max: 1},
+            {id: "smallWindow", min: 0, max: 2},
+            {id: "largeWindow", min: 1, max: 1}
+        ]
     },
     {
         id:"hospital",
         weight:1,
-        areas:{
-            toilet:{min: 4,max: 10},
-            parking:{min: 1,max: 1},
-            kitchen:{min: 1,max: 1},
-            hospitalRoom:{min: 10,max: 50},
-            operatingRoom:{min: 1,max: 5},
-            pharmacy:{min: 1,max: 2},
-            offices:{min: 1,max: 1}
-        }
+        areas:[
+            {id:"toilet",min: 4,max: 10},
+            {id:"parking",min: 1,max: 1},
+            {id:"kitchen",min: 1,max: 1},
+            {id:"hospitalRoom",min: 10,max: 50},
+            {id:"operatingRoom",min: 1,max: 5},
+            {id:"pharmacy",min: 1,max: 2},
+            {id:"offices",min: 1,max: 1}
+        ],
+        entrances:[
+            {id: "frontDoor", min: 1, max: 2},
+            {id: "backDoor", min: 1, max: 1},
+            {id: "garageDoor", min: 1, max: 1},
+            {id: "smallWindow", min: 4, max: 12},
+            {id: "largeWindow", min: 1, max: 2}
+        ]
     },
     {
         id:"policeStation",
         weight:1,
-        areas:{
-            cells:{min: 1,max: 5},
-            armory:{min: 1,max: 1},
-            toilet:{min: 1,max: 2},
-            offices:{min: 1,max: 1}
-        }
+        areas:[
+            {id:"cells",min: 1,max: 5},
+            {id:"armory",min: 1,max: 1},
+            {id:"toilet",min: 1,max: 2},
+            {id:"offices",min: 1,max: 1}
+        ],
+        entrances:[
+            {id: "frontDoor", min: 1, max: 2},
+            {id: "backDoor", min: 0, max: 1},
+            {id: "smallWindow", min: 1, max: 4},
+            {id: "largeWindow", min: 0, max: 1}
+        ]
     },
     {
         id:"fireStation",
         weight:1,
-        areas:{
-            toilet:{min: 1,max: 2},
-            offices:{min: 1,max: 1},
-            garage:{min: 1,max: 1},
-            kitchen:{min: 1,max: 1}
-        }
+        areas:[
+            {id:"toilet",min: 1,max: 2},
+            {id:"offices",min: 1,max: 1},
+            {id:"garage",min: 1,max: 1},
+            {id:"kitchen",min: 1,max: 1}
+        ],
+        entrances:[
+            {id: "frontDoor", min: 1, max: 1},
+            {id: "backDoor", min: 1, max: 1},
+            {id: "garageDoor", min: 1, max: 2},
+            {id: "smallWindow", min: 2, max: 6},
+            {id: "largeWindow", min: 0, max: 1}
+        ]
     },
     {
         id:"park",
         weight:5,
-        areas:{
-            toilet:{min: 0,max: 2},
-            kiosk:{min: 0,max: 2},
-            gardeningShack:{min: 1,max: 1}
-        }
+        areas:[
+            {id:"toilet",min: 0,max: 2},
+            {id:"kiosk",min: 0,max: 2},
+            {id:"gardeningShack",min: 1,max: 1}
+        ],
+        entrances:[]
     },
     {
         id:"gasStation",
         weight:0,
-        areas:{
-            toilet:{min: 2,max: 4},
-            offices:{min: 1,max: 1},
-            parking:{min: 1,max: 1},
-            kitchen:{min: 1,max: 1},
-            kiosk:{min: 1,max: 1}
-        }
+        areas:[
+            {id:"toilet",min: 2,max: 4},
+            {id:"offices",min: 1,max: 1},
+            {id:"parking",min: 1,max: 1},
+            {id:"kitchen",min: 1,max: 1},
+            {id:"kiosk",min: 1,max: 1}
+        ],
+        entrances:[
+            {id: "frontDoor", min: 1, max: 2},
+            {id: "backDoor", min: 1, max: 1},
+            {id: "smallWindow", min: 1, max: 4},
+            {id: "largeWindow", min: 1, max: 2}
+        ]
     }
 ]
 
